ff3da5eec8d47151af6ad759038cbdae40779877
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-19 06:12:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,13 +5,9 @@ end
 def introduction_with_language(name,language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
-def introduction_with_language_optional(name=Andy,language=Ruby)
+def introduction_with_language_optional(name,language="Ruby")
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction_with_language_optional(Andy,Python)
-
-
 
 
 
